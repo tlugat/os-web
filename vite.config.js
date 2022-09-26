434944d757a2a649bb750967be5a2323d6acded9
tlugat <teo.lugat@hetic.net>
2022-09-26 12:03:33 +0200

    fix-config: fix src folder name

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite"
-import { resolve } from "path"
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    root: resolve("sources"),
-    server: {
-        port: 8000,
-        host: "0.0.0.0"
-    }
-})
+	root: resolve("src"),
+	server: {
+		port: 8000,
+		host: "0.0.0.0",
+	},
+});
