4f0c0f995ce77d12b81dcab4a14487a688fe61ae
tlugat <teo.lugat@hetic.net>
2022-09-26 16:13:15 +0200

    fix(config): move index.html into src

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-	root: resolve("src"),
+	root: resolve("/src/index.ts"),
 	server: {
 		port: 8000,
 		host: "0.0.0.0",
