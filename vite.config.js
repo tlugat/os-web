47783de415737ccd1b52f999f499400256ae202a
tlugat <teo.lugat@hetic.net>
2022-09-26 16:17:54 +0200

    fix(config): add dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-	root: resolve("/src/index.ts"),
+	root: resolve("src/"),
 	server: {
 		port: 8000,
 		host: "0.0.0.0",
